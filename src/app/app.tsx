6abd18fa357bb668813ff93c9eba0606f54d8e8e
Denis <den.melnickov2001@yandex.ru>
2025-09-20 17:19:33 +0400

    Исправлен баг с фоном, фон отображается равномерно сверху и снизу

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -6,25 +6,27 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 export function App({ Component, pageProps }: AppProps) {
   return (
     <AppProvider>
-      <div className={`relative min-h-screen overflow-hidden ${montserrat.className}`}>
+      <div
+        className={`relative min-h-screen overflow-hidden ${montserrat.className}`}
+      >
         <div className="absolute inset-0 -z-10">
           {/* Верхний овал */}
           <div
-            className="absolute top-[-169px] h-[338px] w-[754px] rounded-full bg-[#C1D5F1] blur-[100px] 
-            left-1/2 -translate-x-1/2" // <-- Изменено
+            className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 
+               h-[338px] w-[754px] rounded-full bg-[#C1D5F1] blur-[100px]"
           />
           {/* Средний овал */}
           <div
-            className="absolute top-[131px] h-[620px] w-[754px] rounded-full bg-[#E4E4E4] blur-[100px]
-            left-1/2 -translate-x-1/2" // <-- Изменено
+            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
+               h-[620px] w-[754px] rounded-full bg-[#E4E4E4] blur-[100px]"
           />
           {/* Нижний овал */}
           <div
-            className="absolute top-[763px] h-[338px] w-[754px] rounded-full bg-[#B8D3F9] blur-[100px]
-            left-1/2 -translate-x-1/2" // <-- Изменено
+            className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 
+               h-[338px] w-[754px] rounded-full bg-[#B8D3F9] blur-[100px]"
           />
         </div>
-        
+
         <Component {...pageProps} />
       </div>
     </AppProvider>
