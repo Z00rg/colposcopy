d8e3648dbdf83b62d25bcf95021e8640e29090c7
Denis <den.melnickov2001@yandex.ru>
2025-09-20 17:44:01 +0400

    Сверстан спиннер, спиннер для загрузки странички и линк

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import { UiButton } from "@/shared/ui/ui-button";
 import { UiHeader } from "@/shared/ui/ui-header";
+import { UiLink } from "@/shared/ui/ui-link";
+// import { UiPageSpinner } from "@/shared/ui/ui-page-spinner";
+import { UiSpinner } from "@/shared/ui/ui-spinner";
 
 export default function HomePage() {
   return (
       <div className={`flex justify-center items-center min-h-screen flex-col gap-10`}>
         <UiHeader variant="logo"/>
         <UiButton>Начать</UiButton>
+        <UiSpinner/>
+        <UiLink href={"/"}>Ссылка</UiLink>
+        {/* <UiPageSpinner/> */}
       </div>
   );
 }
--- src/shared/ui/ui-spinner.tsx
+++ src/shared/ui/ui-spinner.tsx
@@ -1,7 +1,9 @@
+import clsx from "clsx";
+
 export function UiSpinner({className}: { className?: string}) {
   return (
     <svg
-      className={className}
+      className={clsx(className, "text-[#7BAEE4]")}
       xmlns="http://www.w3.org/2000/svg"
       width="32"
       height="32"
