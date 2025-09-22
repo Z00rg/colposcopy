f945d38a078b6983c7d15852a2f0e7c43986ff68
Denis <den.melnickov2001@yandex.ru>
2025-09-22 16:58:35 +0300

    Сверстана авторизация под смартфоны 6.1 - 7

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -5,15 +5,15 @@ import { UiTextField } from "@/shared/ui/ui-text-field";
 
 export function SignInPage() {
   return (
-    <div className="flex items-center min-h-screen flex-col">
-      <UiHeader variant="logo" className="mt-24" />
-      <form className="flex flex-col justify-center mt-20">
+    <div className="flex flex-col min-h-screen items-center justify-center">
+      <UiHeader variant="logo" className="mt-16" />
+      <form className="flex flex-col justify-center mt-20 flex-1">
         <div className="flex flex-col justify-center items-center gap-5">
           <UiTextField placeholder="Email" />
           <UiTextField placeholder="Пароль" />
           <UiLink href={"/"}>Забыли пароль?</UiLink>
         </div>
-        <div className="flex flex-col justify-center">
+        <div className="flex flex-col justify-center mt-auto mb-10 items-center">
           <UiButton>Начать</UiButton>
           <div className="text-[20px] text-white mt-8">
             Нет аккаунта? <UiLink href={"/"}>Зарегистрируйся</UiLink>
