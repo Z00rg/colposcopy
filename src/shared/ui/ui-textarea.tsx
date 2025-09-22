e9ef89130308082f14e0121fad5d7af778f665d6
Denis <den.melnickov2001@yandex.ru>
2025-09-22 15:41:07 +0300

    У textarea изменен размер шрифта

--- src/shared/ui/ui-textarea.tsx
+++ src/shared/ui/ui-textarea.tsx
@@ -7,7 +7,7 @@ export type UiTextAreaProps = {
 
 export function UiTextArea({className, children} : UiTextAreaProps) {
     return (
-        <div className={clsx(className, "min-w-[300px] max-w-[388px] h-[448px] text-[13px] font-normal bg-[#F3F3F3] flex flex-col items-center pt-3 px-[17px] shadow-[0px_4px_4px_rgba(0,0,0,0.25)] rounded-[20px]",
+        <div className={clsx(className, "min-w-[300px] max-w-[388px] h-[285px] text-[16px] font-normal bg-[#F1F1F1] flex flex-col items-center pt-3 px-[17px] shadow-[0px_4px_4px_rgba(0,0,0,0.25)] rounded-[20px]",
             "overflow-y-auto overflow-x-hidden"
         )}>
             {children}
