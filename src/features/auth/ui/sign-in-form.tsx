c53ba96b41e04fd6716fad3f566d087825dc918e
Denis <den.melnickov2001@yandex.ru>
2025-09-23 21:32:53 +0400

    Сделана логика формочки для авторизации

---
+++ src/features/auth/ui/sign-in-form.tsx
@@ -0,0 +1,9 @@
+import { useSignInForm } from "../model/use-sign-in-form";
+
+export function SignInForm() {
+    const { register, handleSubmit, errorMessage, isPending } = useSignInForm();
+
+    return (
+        
+    );
+}
