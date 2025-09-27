215c9e87164079bb6754feb65358668f3c1b2e7f
Denis <den.melnickov2001@yandex.ru>
2025-09-27 18:31:34 +0400

    Сделан protected-page, требует проверки

--- src/features/auth/ui/protected-page.tsx
+++ src/features/auth/ui/protected-page.tsx
@@ -1,4 +1,5 @@
 // import { useSessionQuery } from "@/entities/session";
+import { useSessionQuery } from "@/entities/session";
 import { ROUTES } from "@/shared/constants/routes";
 import { UiPageSpinner } from "@/shared/ui/ui-page-spinner";
 import { useRouter } from "next/router";
@@ -8,15 +9,15 @@ export function protectedPage<P>(Component: (props: P) => ReactElement) {
   return function ProtectedPage(props: PropsWithChildren<P>) {
     const router = useRouter();
 
-    // const { isLoading, isError } = useSessionQuery();
+    const { isLoading, isError } = useSessionQuery();
 
-    // if (isLoading) {
-    //   return <UiPageSpinner />;
-    // }
+    if (isLoading) {
+      return <UiPageSpinner />;
+    }
 
-    // if (isError) {
-    //   router.replace(ROUTES.SIGN_IN);
-    // }
+    if (isError) {
+      router.replace(ROUTES.SIGN_IN);
+    }
 
     return <Component {...props} />;
   };
