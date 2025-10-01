e914d09ecc7f65bca21b141b4fe948bf0247f3f9
Denis <den.melnickov2001@yandex.ru>
2025-10-01 15:20:30 +0300

    Выравнены расстояния между элементами + доделан скролл имг

--- src/pages/pathology.tsx
+++ src/pages/pathology.tsx
@@ -6,14 +6,14 @@ import { UiScrollImg } from "@/shared/ui/ui-scroll-img";
 import { UiTextArea } from "@/shared/ui/ui-textarea";
 
 export function PathologyPage() {
-  const imgContainer = ["/logo.png", "/logo.png", "/logo.png", "/logo.png"];
+  const imgContainer = ["/test.jpg", "/test.jpg", "/test.jpg", "/test.jpg"];
 
   return (
     <div className="flex flex-col items-center min-h-screen lg:min-h-[667px]">
-      <UiHeader variant="withoutLogo" className="mt-16" />
-      <div className="flex flex-col justify-center items-center gap-3 flex-1 mb-4 px-5">
-        <UiScrollImg img={imgContainer} />
-        <UiTextArea>
+      <UiHeader variant="withoutLogo" className="mt-6" />
+      <div className="flex flex-col justify-center items-center gap-3 flex-1 mb-4 px-5 mt-5">
+        <UiScrollImg className="pt-6" img={imgContainer} />
+        <UiTextArea className="mt-5">
           Зона трансформации (3Т) 1го типа характеризуется полной визуализацией
           всей площади стыка многослойного плоского эпителия и цилиндрического
           эпителия, включая его наиболее важный для скрининга компонент —
--- src/shared/ui/ui-scroll-img.tsx
+++ src/shared/ui/ui-scroll-img.tsx
@@ -9,21 +9,19 @@ type UiScrollImgProps = {
 export function UiScrollImg({ img, className }: UiScrollImgProps) {
   return (
     <div
-      className={clsx(className,
-        "flex snap-x snap-mandatory overflow-x-auto gap-4 scrollbar-hide",
-        "w-[300px] h-[345px] items-center",
+      className={clsx(
+        className,
+        "flex snap-x snap-mandatory overflow-x-auto overflow-y-hidden gap-4 scrollbar-hide",
+        "w-[300px] h-[345px] items-center rounded-2xl shadow-[0px_4px_4px_rgba(0,0,0,0.25)]"
       )}
     >
       {img.map((src, index) => (
-        <div
-          key={"img " + index}
-          className="flex-shrink-0 snap-center"
-        >
+        <div key={"img " + index} className="flex-shrink-0 snap-center">
           <Image
             src={src}
             alt={`Image ${index + 1}`}
             width={300}
-            height={345} 
+            height={345}
           />
         </div>
       ))}
