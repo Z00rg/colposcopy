123b40b15be30d9f9b8ca74f7b6268177b003a61
Denis <den.melnickov2001@yandex.ru>
2025-10-01 14:56:06 +0300

    Добавлен новый чекбокс

--- src/shared/ui/ui-list-button-test.tsx
+++ src/shared/ui/ui-list-button-test.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import Checkbox from "./ui-checkbox";
 
 type PathologyInformation = {
   id: number;
@@ -27,7 +28,25 @@ export function UiListButtonTest({
         {informationOfPathology.name}
       </div>
       <div className="ml-auto w-6 h-6 flex justify-center items-center">
-        <input className="w-5 h-5" type="checkbox" />
+        <input
+          type="checkbox"
+          className="
+    w-5 h-5 
+    appearance-none 
+    border border-gray-400 rounded 
+    transition duration-200 ease-in-out
+    transform 
+    checked:scale-110 
+    checked:bg-[#63a0e2] 
+    checked:border-[#7BAEE4]
+    checked:[&:after]:content-['✔'] 
+    checked:[&:after]:text-white 
+    checked:[&:after]:text-xs
+    checked:[&:after]:flex 
+    checked:[&:after]:items-center 
+    checked:[&:after]:justify-center
+  "
+        />
       </div>
     </div>
   );
