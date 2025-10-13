6d7bd945ed6f3c790893c78f88505df9e6b189a5
Denis <den.melnickov2001@yandex.ru>
2025-10-13 22:00:32 +0400

    Закоментирована часть для сервака, для тестов

--- src/features/atlas/ui/pathology-detail.tsx
+++ src/features/atlas/ui/pathology-detail.tsx
@@ -47,53 +47,53 @@ export function PathologyDetail ({ className }: {className?: string}) {
       }
 
     return (
-    // <div className={clsx(className, "flex flex-col justify-center items-center gap-3 flex-1 mb-4 px-5 mt-5")}>
-    //         <UiScrollImg img={imgContainer} onIndexChange={handleImageChange} />
+    <div className={clsx(className, "flex flex-col justify-center items-center gap-3 flex-1 mb-4 px-5 mt-5")}>
+            <UiScrollImg img={imgContainer} onIndexChange={handleImageChange} />
     
-    //         {currentImageIndex === 3 ? (
-    //           <Image
-    //             src={imgContainer[4]}
-    //             alt="Test image"
-    //             width={385}
-    //             height={285}
-    //             className="rounded-xl object-contain mt-5"
-    //           />
-    //         ) : (
-    //           <UiTextArea className="mt-5">
-    //             {textContainer[currentImageIndex]}
-    //           </UiTextArea>
-    //         )}
-    //         <UiLink href={ROUTES.ATLAS} className="mr-auto">
-    //           Назад
-    //         </UiLink>
-    //         <UiFooter activeStatus="atlas" />
-    //       </div> 
-          <div className={clsx(className, "flex flex-col justify-center items-center gap-3 flex-1 mb-4 px-5 mt-5")}>
-            {isLoading && <UiSpinner/>}
-            {isError && <div className="font-bold text-rose-500">Ошибка при загрузке деталей патологии</div>}
-            {pathologyDetails && (
-              <>
-                <UiScrollImg img={pathologyDetails.imgContainer} onIndexChange={handleImageChange} />
-                {currentImageIndex === 3 ? (
-                  <Image
-                    src={pathologyDetails.imgContainer[4]}
-                    alt="Test image"
-                    width={385}
-                    height={285}
-                    className="rounded-xl object-contain mt-5"
-                  />
-                ) : (
-                  <UiTextArea className="mt-5">
-                    {pathologyDetails.textContainer[currentImageIndex]}
-                  </UiTextArea>
-                )}
-                <UiLink href={ROUTES.ATLAS} className="mr-auto">
-                  Назад
-                </UiLink>
-                <UiFooter activeStatus="atlas" />
-              </>
+            {currentImageIndex === 3 ? (
+              <Image
+                src={imgContainer[4]}
+                alt="Test image"
+                width={385}
+                height={285}
+                className="rounded-xl object-contain mt-5"
+              />
+            ) : (
+              <UiTextArea className="mt-5">
+                {textContainer[currentImageIndex]}
+              </UiTextArea>
             )}
+            <UiLink href={ROUTES.ATLAS} className="mr-auto">
+              Назад
+            </UiLink>
+            <UiFooter activeStatus="atlas" />
+          </div> 
+        //   <div className={clsx(className, "flex flex-col justify-center items-center gap-3 flex-1 mb-4 px-5 mt-5")}>
+        //     {isLoading && <UiSpinner/>}
+        //     {isError && <div className="font-bold text-rose-500">Ошибка при загрузке деталей патологии</div>}
+        //     {pathologyDetails && (
+        //       <>
+        //         <UiScrollImg img={pathologyDetails.imgContainer} onIndexChange={handleImageChange} />
+        //         {currentImageIndex === 3 ? (
+        //           <Image
+        //             src={pathologyDetails.imgContainer[4]}
+        //             alt="Test image"
+        //             width={385}
+        //             height={285}
+        //             className="rounded-xl object-contain mt-5"
+        //           />
+        //         ) : (
+        //           <UiTextArea className="mt-5">
+        //             {pathologyDetails.textContainer[currentImageIndex]}
+        //           </UiTextArea>
+        //         )}
+        //         <UiLink href={ROUTES.ATLAS} className="mr-auto">
+        //           Назад
+        //         </UiLink>
+        //         <UiFooter activeStatus="atlas" />
+        //       </>
+        //     )}
             
-          </div>
+        //   </div>
           )
 }
