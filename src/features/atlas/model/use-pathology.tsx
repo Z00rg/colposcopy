0360eada1d21cfec8fbf5fe81a81e77925f55ece
Denis <den.melnickov2001@yandex.ru>
2025-10-15 13:46:16 +0300

    Разделена логика и верстка на страничке паталогии

--- src/features/atlas/model/use-pathology.tsx
+++ src/features/atlas/model/use-pathology.tsx
@@ -1,11 +1,25 @@
 import { usePathologyQuery } from "@/entities/pathology";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
-export function usePathology(pathologyId: string) {
-    const pathologyQuery = usePathologyQuery(pathologyId);
+export function usePathology() {
+  const router = useRouter();
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-    return {
-        pathologyDetails: pathologyQuery.data,
-        isLoading: pathologyQuery.isPending,
-        isError: pathologyQuery.isError
-    }
+  const { pathologyId } = router.query;
+  const validPathologyId =
+    typeof pathologyId === "string" ? pathologyId : undefined;
+  const pathologyQuery = usePathologyQuery(validPathologyId as string);
+
+  const handleImageChange = (index: number) => {
+    setCurrentImageIndex(index);
+  };
+
+  return {
+    pathologyDetails: pathologyQuery.data,
+    isLoading: pathologyQuery.isPending,
+    isError: pathologyQuery.isError,
+    currentImageIndex,
+    handleImageChange,
+  };
 }
--- src/features/atlas/ui/pathology.tsx
+++ src/features/atlas/ui/pathology.tsx
@@ -1,100 +1,91 @@
 import { UiScrollImg } from "@/shared/ui/ui-scroll-img";
-import { useRouter } from "next/router";
-import { useState } from "react";
 import Image from "next/image";
 import { UiTextArea } from "@/shared/ui/ui-textarea";
 import { UiLink } from "@/shared/ui/ui-link";
 import { UiFooter } from "@/shared/ui/ui-footer";
 import { ROUTES } from "@/shared/constants/routes";
 import clsx from "clsx";
-import { UiSpinner } from "@/shared/ui/ui-spinner";
 import { usePathology } from "../model/use-pathology";
 
-export function Pathology ({ className }: {className?: string}) {
-      const router = useRouter();
-      
-    const { pathologyId } = router.query; 
+export function Pathology({ className }: { className?: string }) {
+  const {
+    pathologyDetails,
+    isLoading,
+    isError,
+    currentImageIndex,
+    handleImageChange,
+  } = usePathology();
 
-    const validPathologyId = typeof pathologyId === "string" ? pathologyId : undefined;
-
-    const { pathologyDetails, isLoading, isError } = usePathology(validPathologyId as string);  
-      
-      const [currentImageIndex, setCurrentImageIndex] = useState(0);
-    
-      const imgContainer = ["/test.jpg", "/test.jpg", "/test.jpg", "/test.jpg"];
-      const imgSchema = "/imageFourth.png";
-      const textContainer = [
-        `Картинка 1: Зона трансформации (3Т) 1го типа характеризуется полной визуализацией
+  const imgContainer = ["/test.jpg", "/test.jpg", "/test.jpg", "/test.jpg"];
+  const imgSchema = "/imageFourth.png";
+  const textContainer = [
+    `Картинка 1: Зона трансформации (3Т) 1го типа характеризуется полной визуализацией
               всей площади стыка многослойного плоского эпителия и цилиндрического
               эпителия, включая его наиболее важный для скрининга компонент —
               границу метаплазии, расположенную на эктоцервиксе.`,
-        `Картинка 2: Зона трансформации (3Т) 1го типа характеризуется полной визуализацией
+    `Картинка 2: Зона трансформации (3Т) 1го типа характеризуется полной визуализацией
               всей площади стыка многослойного плоского эпителия и цилиндрического
               эпителия, включая его наиболее важный для скрининга компонент —
               границу метаплазии, расположенную на эктоцервиксе.`,
-        `Картинка 3: Зона трансформации (3Т) 1го типа характеризуется полной визуализацией
+    `Картинка 3: Зона трансформации (3Т) 1го типа характеризуется полной визуализацией
               всей площади стыка многослойного плоского эпителия и цилиндрического
               эпителия, включая его наиболее важный для скрининга компонент —
               границу метаплазии, расположенную на эктоцервиксе.`,
-      ];
+  ];
 
-      const handleImageChange = (index: number) => {
-        setCurrentImageIndex(index);
-      };
+  return (
+    <div
+      className={clsx(
+        className,
+        "flex flex-col justify-center items-center gap-3 flex-1 mb-4 px-5 mt-5"
+      )}
+    >
+      <UiScrollImg img={imgContainer} onIndexChange={handleImageChange} />
 
-      return (
-        <div
-          className={clsx(
-            className,
-            "flex flex-col justify-center items-center gap-3 flex-1 mb-4 px-5 mt-5"
-          )}
-        >
-          <UiScrollImg img={imgContainer} onIndexChange={handleImageChange} />
+      {currentImageIndex === 3 ? (
+        <Image
+          src={imgSchema}
+          alt="Test image"
+          width={385}
+          height={285}
+          className="rounded-xl object-contain mt-5"
+        />
+      ) : (
+        <UiTextArea className="mt-5">
+          {textContainer[currentImageIndex]}
+        </UiTextArea>
+      )}
+      <UiLink href={ROUTES.ATLAS} className="mr-auto">
+        Назад
+      </UiLink>
+      <UiFooter activeStatus="atlas" />
+    </div>
+    //   <div className={clsx(className, "flex flex-col justify-center items-center gap-3 flex-1 mb-4 px-5 mt-5")}>
+    //     {isLoading && <UiSpinner/>}
+    //     {isError && <div className="font-bold text-rose-500">Ошибка при загрузке деталей патологии</div>}
+    //     {pathologyDetails && (
+    //       <>
+    //         <UiScrollImg img={pathologyDetails.imgContainer} onIndexChange={handleImageChange} />
+    //         {currentImageIndex === 3 ? (
+    //           <Image
+    //             src={pathologyDetails.imgSchema}
+    //             alt="Схематическое изображение"
+    //             width={385}
+    //             height={285}
+    //             className="rounded-xl object-contain mt-5"
+    //           />
+    //         ) : (
+    //           <UiTextArea className="mt-5">
+    //             {pathologyDetails.textContainer[currentImageIndex]}
+    //           </UiTextArea>
+    //         )}
+    //         <UiLink href={ROUTES.ATLAS} className="mr-auto">
+    //           Назад
+    //         </UiLink>
+    //         <UiFooter activeStatus="atlas" />
+    //       </>
+    //     )}
 
-          {currentImageIndex === 3 ? (
-            <Image
-              src={imgSchema}
-              alt="Test image"
-              width={385}
-              height={285}
-              className="rounded-xl object-contain mt-5"
-            />
-          ) : (
-            <UiTextArea className="mt-5">
-              {textContainer[currentImageIndex]}
-            </UiTextArea>
-          )}
-          <UiLink href={ROUTES.ATLAS} className="mr-auto">
-            Назад
-          </UiLink>
-          <UiFooter activeStatus="atlas" />
-        </div>
-        //   <div className={clsx(className, "flex flex-col justify-center items-center gap-3 flex-1 mb-4 px-5 mt-5")}>
-        //     {isLoading && <UiSpinner/>}
-        //     {isError && <div className="font-bold text-rose-500">Ошибка при загрузке деталей патологии</div>}
-        //     {pathologyDetails && (
-        //       <>
-        //         <UiScrollImg img={pathologyDetails.imgContainer} onIndexChange={handleImageChange} />
-        //         {currentImageIndex === 3 ? (
-        //           <Image
-        //             src={pathologyDetails.imgSchema}
-        //             alt="Схематическое изображение"
-        //             width={385}
-        //             height={285}
-        //             className="rounded-xl object-contain mt-5"
-        //           />
-        //         ) : (
-        //           <UiTextArea className="mt-5">
-        //             {pathologyDetails.textContainer[currentImageIndex]}
-        //           </UiTextArea>
-        //         )}
-        //         <UiLink href={ROUTES.ATLAS} className="mr-auto">
-        //           Назад
-        //         </UiLink>
-        //         <UiFooter activeStatus="atlas" />
-        //       </>
-        //     )}
-
-        //   </div>
-      );
+    //   </div>
+  );
 }
