4c1e3006ef09f7df762f3ef8054dfe26d1242bc7
Denis <den.melnickov2001@yandex.ru>
2025-10-15 13:27:06 +0300

    Раздлена логика и верстка списка тестов

--- src/features/test/model/use-test-list.tsx
+++ src/features/test/model/use-test-list.tsx
@@ -1,13 +1,54 @@
 import { useTestListQuery } from "@/entities/test-list";
+import { ROUTES } from "@/shared/constants/routes";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 export function useTestList() {
-    const testListQuery = useTestListQuery();
+  const router = useRouter();
 
-    const items = testListQuery.data?.items ?? [];
+  // Состояние для хранения ID
+  const [selectedIds, setSelectedIds] = useState<number[]>([]);
 
-    return {
-        items,
-        isLoading: testListQuery.isPending,
-        isError: testListQuery.isError
+  // Функция для переключения выбора патологии
+  const handleTogglePathology = (id: number) => {
+    setSelectedIds((prevIds) => {
+      if (prevIds.includes(id)) {
+        // Удалить ID
+        return prevIds.filter((selectedId) => selectedId !== id);
+      } else {
+        // Добавить ID
+        return [...prevIds, id];
+      }
+    });
+  };
+
+  // Обработчик начала попытки
+  const handleStartAttempt = () => {
+    if (selectedIds.length === 0) {
+      alert("Пожалуйста, выберите хотя бы одну патологию для теста.");
+      return;
     }
+
+    // Преобразуем массив в строку
+    const idsString = selectedIds.join(",");
+
+    // Навигация с параметром запроса (Query Parameter)
+    router.push({
+      pathname: ROUTES.PASSING,
+      query: { testIds: idsString },
+    });
+  };
+
+  const testListQuery = useTestListQuery();
+
+  const items = testListQuery.data?.items ?? [];
+
+  return {
+    items,
+    isLoading: testListQuery.isPending,
+    isError: testListQuery.isError,
+    selectedIds,
+    handleTogglePathology,
+    handleStartAttempt,
+  };
 }
--- src/features/test/ui/test-list.tsx
+++ src/features/test/ui/test-list.tsx
@@ -1,16 +1,19 @@
-import { ROUTES } from "@/shared/constants/routes";
 import { UiList } from "@/shared/ui/ui-list";
 import { UiListButtonTest } from "@/shared/ui/ui-list-button-test";
-import { useRouter } from "next/router";
-import { useState } from "react";
 import { Instruction } from "./instruction";
 import clsx from "clsx";
+import { useTestList } from "../model/use-test-list";
+import { UiSpinner } from "@/shared/ui/ui-spinner";
 
 export function TestList() {
-  const router = useRouter();
-
-  // Состояние для хранения ID
-  const [selectedIds, setSelectedIds] = useState<number[]>([]);
+  const {
+    items,
+    isLoading,
+    isError,
+    selectedIds,
+    handleTogglePathology,
+    handleStartAttempt,
+  } = useTestList();
 
   const informationOfPathology = [
     { id: 1, name: "Зона трансформации 1 типа A" },
@@ -20,36 +23,6 @@ export function TestList() {
     { id: 5, name: "Зона трансформации 1 типа E" },
     { id: 6, name: "Зона трансформации 2 типа F" },
   ];
-
-  // Функция для переключения выбора патологии
-  const handleTogglePathology = (id: number) => {
-    setSelectedIds((prevIds) => {
-      if (prevIds.includes(id)) {
-        // Удалить ID
-        return prevIds.filter((selectedId) => selectedId !== id);
-      } else {
-        // Добавить ID
-        return [...prevIds, id];
-      }
-    });
-  };
-
-  // Обработчик начала попытки
-  const handleStartAttempt = () => {
-    if (selectedIds.length === 0) {
-      alert("Пожалуйста, выберите хотя бы одну патологию для теста.");
-      return;
-    }
-
-    // Преобразуем массив в строку
-    const idsString = selectedIds.join(",");
-
-    // Навигация с параметром запроса (Query Parameter)
-    router.push({
-      pathname: ROUTES.PASSING,
-      query: { testIds: idsString },
-    });
-  };
 
   return (
     <UiList className="mt-4 items-start max-h-[530px]">
@@ -81,5 +54,41 @@ export function TestList() {
         Начать попытку
       </button>
     </UiList>
+    // <UiList className="mt-4 items-start max-h-[530px]">
+    //   <div className="font-bold text-[18px] text-[#4B4242]">
+    //     Выберите параметры
+    //   </div>
+    //   {isLoading && <UiSpinner />}
+    //   {isError && (
+    //     <div className="font-bold text-rose-500">
+    //       Ошибка при загрузке списка патологий
+    //     </div>
+    //   )}
+    //   {items &&
+    //     items.map((item, index) => (
+    //       <UiListButtonTest
+    //         className="w-full"
+    //         key={item.id}
+    //         index={index + 1}
+    //         informationOfPathology={item}
+    //         isChecked={selectedIds.includes(item.id)}
+    //         onToggle={handleTogglePathology}
+    //       />
+    //     ))}
+    //   <div className="flex w-full border border-b-1 border-[#BDBDBD]"></div>
+    //   <Instruction />
+    //   <button
+    //     onClick={handleStartAttempt}
+    //     disabled={selectedIds.length === 0}
+    //     className={clsx(
+    //       "ml-auto text-[20px] mb-3 font-normal transition-colors",
+    //       selectedIds.length === 0
+    //         ? "text-gray-400 cursor-not-allowed"
+    //         : "text-[#2E76AA] hover:text-[#26628A] cursor-pointer"
+    //     )}
+    //   >
+    //     Начать попытку
+    //   </button>
+    // </UiList>
   );
 }
