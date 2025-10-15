7257e5976bddb6ad5e3215e0c95f2bd37dd8528d
Denis <den.melnickov2001@yandex.ru>
2025-10-15 11:17:00 +0300

    Переделаны принимаемые пропсы для чекбокса

--- src/shared/ui/ui-checkbox.tsx
+++ src/shared/ui/ui-checkbox.tsx
@@ -1,8 +1,16 @@
-export function UiCheckBox() {
+import * as React from "react";
+import clsx from "clsx";
+
+export type UiCheckBoxProps = React.ComponentPropsWithoutRef<"input">;
+
+export function UiCheckBox({ className, ...props }: UiCheckBoxProps) {
   return (
     <input
+      {...props}
       type="checkbox"
-      className="
+      className={clsx(
+        className,
+        `
             w-5 h-5 
             appearance-none 
             border border-gray-400 rounded 
@@ -17,7 +25,8 @@ export function UiCheckBox() {
             checked:[&:after]:flex 
             checked:[&:after]:items-center 
             checked:[&:after]:justify-center
-          "
+          `
+      )}
     />
   );
 }
