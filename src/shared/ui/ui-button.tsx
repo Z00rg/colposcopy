0caaae89acf168e9e3f0068e2f794616d3062700
Denis <den.melnickov2001@yandex.ru>
2025-11-06 19:01:11 +0400

    Добавлена анимация наведения на кнопку

--- src/shared/ui/ui-button.tsx
+++ src/shared/ui/ui-button.tsx
@@ -10,7 +10,7 @@ export function UiButton({ className, ...props }: UiButtonProps) {
       {...props}
       className={clsx(
         className,
-        "w-[323px] h-[69px] rounded-[20px] cursor-pointer flex items-center justify-center text-3xl bg-[#7BAEE4] hover:bg-[#6899D2] text-white shadow-[0px_30px_30px_rgba(0,0,0,0.2)]"
+        "w-[323px] h-[69px] rounded-[20px] cursor-pointer flex items-center justify-center text-3xl bg-[#7BAEE4] hover:bg-[#6899D2] transition delay-150 duration-300 text-white shadow-[0px_30px_30px_rgba(0,0,0,0.2)]"
       )}
     />
   );
