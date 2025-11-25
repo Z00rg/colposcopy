584fbe5b0456aa0c9148d45a7ae92b2b60a8f1e7
Denis <den.melnickov2001@yandex.ru>
2025-11-25 19:14:46 +0400

    Доделан список клинических случаев

--- src/features/clinical-case/ui/clinical-cases-list.tsx
+++ src/features/clinical-case/ui/clinical-cases-list.tsx
@@ -1,85 +1,53 @@
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import { UiList } from "@/shared/ui/ui-list";
-// import { useAtlasList } from "../model/use-atlas-list";
 import { UiSpinner } from "@/shared/ui/ui-spinner";
 import clsx from "clsx";
-import { UiListButtonClinic } from '@/shared/ui/ui-list-button-clinic';
+import { UiListButtonClinic } from "@/shared/ui/ui-list-button-clinic";
+import { useClinicalCases } from "../model/use-clinical-cases";
 
 export function ClinicalCasesList({ className }: { className?: string }) {
-  // const { items, isLoading, isError, handleClick } = useAtlasList();
-  
+  const { items, isLoading, isError } = useClinicalCases();
 
   const testItems = [
     {
       id: 1,
       name: "Зона трансформации 1 типаЗона трансформации 1 типаЗона трансформации 1 типа",
-      cases: [
-        { id: 1},
-        { id: 2},
-        { id: 3},
-      ]
+      cases: [{ id: 1 }, { id: 2 }, { id: 3 }],
     },
     {
       id: 2,
       name: " Зона трансформации 2 типа",
-      cases: [
-        { id: 4},
-        { id: 5},
-        { id: 6},
-      ]
+      cases: [{ id: 4 }, { id: 5 }, { id: 6 }],
     },
     {
       id: 3,
       name: "Зона трансформации 1 типаЗона трансформации 1 типаЗона трансформации 1 типа",
-      cases: [
-        { id: 7},
-        { id: 8},
-        { id: 9},
-      ]
+      cases: [{ id: 7 }, { id: 8 }, { id: 9 }],
     },
     {
       id: 4,
       name: " Зона трансформации 2 типа",
-      cases: [
-        { id: 10},
-        { id: 11},
-        { id: 12},
-      ]
+      cases: [{ id: 10 }, { id: 11 }, { id: 12 }],
     },
     {
       id: 5,
       name: "Зона трансформации 1 типаЗона трансформации 1 типаЗона трансформации 1 типа",
-      cases: [
-        { id: 13},
-        { id: 14},
-        { id: 15},
-      ]
+      cases: [{ id: 13 }, { id: 14 }, { id: 15 }],
     },
     {
       id: 6,
       name: " Зона трансформации 2 типа",
-      cases: [
-        { id: 16},
-        { id: 17},
-        { id: 18},
-      ]
+      cases: [{ id: 16 }, { id: 17 }, { id: 18 }],
     },
     {
       id: 7,
       name: "Зона трансформации 1 типаЗона трансформации 1 типаЗона трансформации 1 типа",
-      cases: [
-        { id: 19},
-        { id: 20},
-        { id: 21},
-      ]
+      cases: [{ id: 19 }, { id: 20 }, { id: 21 }],
     },
     {
       id: 8,
       name: " Зона трансформации 2 типа",
-      cases: [
-        { id: 22},
-        { id: 23},
-      ]
+      cases: [{ id: 22 }, { id: 23 }],
     },
   ];
 
@@ -93,12 +61,12 @@ export function ClinicalCasesList({ className }: { className?: string }) {
     //   )}
     //   {items &&
     //     items.map((item, index) => (
-    //       <UiListButtonAtlas
+    //       <UiListButtonClinic
     //         className="w-full"
     //         key={item.id}
     //         index={index + 1}
     //         informationOfPathology={item}
-    //         onClick={() => handleClick(item.id)}
+    //         cases={item.cases}
     //       />
     //     ))}
     // </UiList>
