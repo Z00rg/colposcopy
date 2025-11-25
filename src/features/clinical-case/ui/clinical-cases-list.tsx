e6af0c7919870cc12c82b91bfd733e99020f5ce4
Denis <den.melnickov2001@yandex.ru>
2025-11-25 18:45:45 +0400

    Сверстана страничка со списком клинических случаев

--- src/features/clinical-case/ui/clinical-cases-list.tsx
+++ src/features/clinical-case/ui/clinical-cases-list.tsx
@@ -7,39 +7,79 @@ import { UiListButtonClinic } from '@/shared/ui/ui-list-button-clinic';
 
 export function ClinicalCasesList({ className }: { className?: string }) {
   // const { items, isLoading, isError, handleClick } = useAtlasList();
+  
 
   const testItems = [
     {
       id: 1,
       name: "Зона трансформации 1 типаЗона трансформации 1 типаЗона трансформации 1 типа",
+      cases: [
+        { id: 1},
+        { id: 2},
+        { id: 3},
+      ]
     },
     {
       id: 2,
       name: " Зона трансформации 2 типа",
+      cases: [
+        { id: 4},
+        { id: 5},
+        { id: 6},
+      ]
     },
     {
       id: 3,
       name: "Зона трансформации 1 типаЗона трансформации 1 типаЗона трансформации 1 типа",
+      cases: [
+        { id: 7},
+        { id: 8},
+        { id: 9},
+      ]
     },
     {
       id: 4,
       name: " Зона трансформации 2 типа",
+      cases: [
+        { id: 10},
+        { id: 11},
+        { id: 12},
+      ]
     },
     {
       id: 5,
       name: "Зона трансформации 1 типаЗона трансформации 1 типаЗона трансформации 1 типа",
+      cases: [
+        { id: 13},
+        { id: 14},
+        { id: 15},
+      ]
     },
     {
       id: 6,
       name: " Зона трансформации 2 типа",
+      cases: [
+        { id: 16},
+        { id: 17},
+        { id: 18},
+      ]
     },
     {
       id: 7,
       name: "Зона трансформации 1 типаЗона трансформации 1 типаЗона трансформации 1 типа",
+      cases: [
+        { id: 19},
+        { id: 20},
+        { id: 21},
+      ]
     },
     {
       id: 8,
       name: " Зона трансформации 2 типа",
+      cases: [
+        { id: 22},
+        { id: 23},
+      ]
     },
   ];
 
@@ -69,7 +109,7 @@ export function ClinicalCasesList({ className }: { className?: string }) {
           key={item.id}
           index={index + 1}
           informationOfPathology={item}
-          onClick={() => handleClick(item.id)}
+          cases={item.cases}
         />
       ))}
     </UiList>
--- src/shared/ui/ui-list-button-clinic.tsx
+++ src/shared/ui/ui-list-button-clinic.tsx
@@ -1,4 +1,6 @@
 import clsx from "clsx";
+import router from "next/router";
+import { useState } from "react";
 
 type PathologyInformation = {
   id: number;
@@ -9,29 +11,86 @@ export type UiListButtonClinicProps = {
   className?: string;
   index: number;
   informationOfPathology: PathologyInformation;
-  onClick: () => void;
+  cases: { id: number }[];
 };
 
 export function UiListButtonClinic({
   className,
   index,
   informationOfPathology,
-  onClick,
+  cases,
 }: UiListButtonClinicProps) {
+  const [active, setActive] = useState(false);
+
+  const handleCaseClick = (id: number) => {
+    router.push(`/case/${id}`);
+  };
+
   return (
     <div
       className={clsx(
         className,
-        "flex items-center text-[18px] font-medium gap-3 cursor-pointer select-none border-b border-[#E0E0E0] px-3 py-3 rounded-xl transition-all duration-200 ease-out",
-        "hover:bg-blue-50 hover:border-blue-400 hover:shadow-md hover:scale-[1.01]"
+        "flex w-full flex-col select-none border-b border-[#E0E0E0] rounded-xl px-3 py-3 transition-all duration-200 ease-out",
+          "hover:bg-blue-50 hover:border-blue-400 hover:shadow-md hover:scale-[1.01] cursor-pointer"
+       
       )}
-      onClick={onClick}
     >
-      <div className="text-gray-700 font-semibold">{index}.</div>
+      {/* Верхняя строка */}
+      <div
+        className={clsx(
+          "flex items-center text-[18px] font-medium gap-3",
+          "px-3 py-3 rounded-xl"
+        )}
+        onClick={() => setActive(!active)}
+      >
+        <div className="text-gray-700 font-semibold">{index}.</div>
 
-      <div className="break-words whitespace-normal flex-1 text-gray-800">
-        {informationOfPathology.name}
+        <div className="break-words whitespace-normal flex-1 text-gray-800">
+          {informationOfPathology.name}
+        </div>
+        <ArrowRight
+                  className={clsx(
+                    "transition-transform duration-300 ease-in-out w-[11px] h-[18px]",
+                    active && "rotate-90"
+                  )}
+                />
+      </div>
+
+      {/* Раскрывающаяся часть */}
+      <div
+        className={clsx(
+          "transition-all duration-300 ease-in-out overflow-hidden mt-2",
+          active ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"
+        )}
+      >
+        <div className="flex flex-col gap-2 pl-11 pr-2 pb-2">
+          {cases.map((caseItem, idx) => (
+            <div
+              key={caseItem.id}
+              className="text-gray-700 text-[15px] font-medium"
+              onClick={() => handleCaseClick(caseItem.id)}
+            >
+              Случай {idx + 1}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 }
+
+export const ArrowRight = ({ className }: { className?: string }) => (
+  <svg
+    className={className}
+    width="9"
+    height="16"
+    viewBox="0 0 9 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M8.70711 8.70711C9.09763 8.31658 9.09763 7.68342 8.70711 7.29289L2.34315 0.928932C1.95262 0.538408 1.31946 0.538408 0.928932 0.928932C0.538408 1.31946 0.538408 1.95262 0.928932 2.34315L6.58579 8L0.928932 13.6569C0.538408 14.0474 0.538408 14.6805 0.928932 15.0711C1.31946 15.4616 1.95262 15.4616 2.34315 15.0711L8.70711 8.70711ZM6 8V9H8V8V7H6V8Z"
+      fill="black"
+    />
+  </svg>
+);
