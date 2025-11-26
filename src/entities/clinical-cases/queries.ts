70d538e381d4e01b9bc76355388a54f288180c7c
Denis <den.melnickov2001@yandex.ru>
2025-11-26 12:28:49 +0300

    casesApi

--- src/entities/clinical-cases/queries.ts
+++ src/entities/clinical-cases/queries.ts
@@ -1,5 +1,6 @@
-import { casesControllerGetClinicalCasesInfo } from "@/shared/api/api";
-import { casesControllerGetCaseInfo } from "@/shared/api/api";
+// import { casesControllerGetClinicalCasesInfo } from "@/shared/api/api";
+// import { casesControllerGetCaseInfo } from "@/shared/api/api";
+import { casesApi } from "@/shared/api/casesApi";
 import { useQuery } from "@tanstack/react-query";
 
 const clinicalCasesKey = ["clinical-cases"];
@@ -8,7 +9,8 @@ const caseKey = (id: string) => ["case", id];
 export function useClinicalCasesQuery() {
   return useQuery({
     queryKey: clinicalCasesKey,
-    queryFn: casesControllerGetClinicalCasesInfo,
+    // queryFn: casesControllerGetClinicalCasesInfo,
+    queryFn: casesApi.getCaseList,
     retry: 0,
     staleTime: 60 * 60 * 1000,
   });
@@ -17,7 +19,8 @@ export function useClinicalCasesQuery() {
 export function useCaseQuery(caseId: string) {
   return useQuery({
     queryKey: caseKey(caseId),
-    queryFn: () => casesControllerGetCaseInfo(caseId),
+    // queryFn: () => casesControllerGetCaseInfo(caseId),
+    queryFn: () => casesApi.getCaseInfo(caseId),
     staleTime: 5 * 60 * 1000, // 5 минут
     retry: 1,
   });
