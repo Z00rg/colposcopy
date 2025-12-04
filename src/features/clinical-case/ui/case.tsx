3c81cb2ac2c38ccaf587d52dcbb1119c3438b6c2
Denis <den.melnickov2001@yandex.ru>
2025-12-04 20:14:26 +0400

    Удалены лишние комментарии из фичи кейсов

--- src/features/clinical-case/ui/case.tsx
+++ src/features/clinical-case/ui/case.tsx
@@ -17,50 +17,7 @@ export function Case({ className }: { className?: string }) {
     handleImageChange,
   } = useCase();
 
-  // const imgContainer = ["/test.jpg", "/test.jpg", "/test.jpg", "/test.jpg"];
-  // const imgSchema = "/imageFourth.png";
-  // const descriptionContainer = [
-  //   `Картинка 1: Зона трансформации (3Т) 1го типа характеризуется полной визуализацией
-  //             всей площади стыка многослойного плоского эпителия и цилиндрического
-  //             эпителия, включая его наиболее важный для скрининга компонент —
-  //             границу метаплазии, расположенную на эктоцервиксе.`,
-  //   `Картинка 2: Зона трансформации (3Т) 1го типа характеризуется полной визуализацией
-  //             всей площади стыка многослойного плоского эпителия и цилиндрического
-  //             эпителия, включая его наиболее важный для скрининга компонент —
-  //             границу метаплазии, расположенную на эктоцервиксе.`,
-  //   `Картинка 3: Зона трансформации (3Т) 1го типа характеризуется полной визуализацией
-  //             всей площади стыка многослойного плоского эпителия и цилиндрического
-  //             эпителия, включая его наиболее важный для скрининга компонент —
-  //             границу метаплазии, расположенную на эктоцервиксе.`,
-  // ];
-
   return (
-    // <div
-    //   className={clsx(
-    //     className,
-    //     "flex flex-col justify-center items-center gap-3 flex-1 mb-4 px-5 mt-5"
-    //   )}
-    // >
-    //   <UiScrollImg img={imgContainer} onIndexChange={handleImageChange} />
-
-    //   {currentImageIndex === imgContainer.length - 1 ? (
-    //     <Image
-    //       src={imgSchema}
-    //       alt="Test image"
-    //       width={385}
-    //       height={285}
-    //       className="rounded-xl object-contain mt-5"
-    //     />
-    //   ) : (
-    //     <UiTextArea className="mt-5">
-    //       {descriptionContainer[currentImageIndex]}
-    //     </UiTextArea>
-    //   )}
-    //   <UiLink href={ROUTES.CLINIC} className="mr-auto">
-    //     Назад
-    //   </UiLink>
-    //   <UiFooter activeStatus="clinic" />
-    // </div>
       <div className={clsx(className, "flex flex-col justify-center items-center gap-3 flex-1 mb-4 px-5 mt-5")}>
         {isLoading && <UiSpinner/>}
         {isError && <div className="font-bold text-rose-500">Ошибка при загрузке деталей патологии</div>}
