162b15c830c612970373919f114fdb7a7e44043c
Denis <den.melnickov2001@yandex.ru>
2025-12-07 16:34:07 +0400

    1

--- src/shared/ui/ui-textarea.tsx
+++ src/shared/ui/ui-textarea.tsx
@@ -20,7 +20,7 @@ export function UiTextArea({
       ref={textAreaRef}
       className={clsx(
         className,
-        "min-w-[328px] lg:min-w-[371px] max-w-[388px] h-[285px]",
+        "min-w-[328px] lg:min-w-[371px] max-w-[550px] h-[285px]",
         "text-[16px] font-normal bg-[#F1F1F1] pt-3 px-[17px]",
         "shadow-[0px_4px_4px_rgba(0,0,0,0.25)] rounded-[20px]",
         "overflow-y-auto overflow-x-hidden scroll-smooth",
