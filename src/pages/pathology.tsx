15f65ee5d476b5d8071136048daecaf71ae751cc
Denis <den.melnickov2001@yandex.ru>
2025-12-07 21:53:41 +0400

    1

--- src/pages/pathology.tsx
+++ src/pages/pathology.tsx
@@ -1,5 +1,5 @@
 import { Pathology } from "@/features/atlas";
-import { UiHeader } from "@/shared/ui/ui-header";
+// import { UiHeader } from "@/shared/ui/ui-header";
 import Head from "next/head";
 
 export function PathologyDetailPage() {
