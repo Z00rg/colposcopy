38412108e5138fbd8c2c866be3acfbb932b32f5d
Denis <den.melnickov2001@yandex.ru>
2025-12-09 17:58:31 +0400

    Макет для верстки туториала

---
+++ src/features/tutorial/ui/tutorial.tsx
@@ -0,0 +1,43 @@
+import { UiTextArea } from "@/shared/ui/ui-textarea";
+import { UiLink } from "@/shared/ui/ui-link";
+import { UiFooter } from "@/shared/ui/ui-footer";
+import { ROUTES } from "@/shared/constants/routes";
+import clsx from "clsx";
+import { useTutorial } from "../model/use-tutorial";
+import { UiSpinner } from "@/shared/ui/ui-spinner";
+
+export function Tutorial({ className }: { className?: string }) {
+  const {
+    tutorialDetails,
+    isLoading,
+    isError,
+  } = useTutorial();
+
+  return (
+    <div
+      className={clsx(
+        className,
+        "flex flex-col w-full gap-3 flex-1 mb-4 px-5 mt-5"
+      )}
+    >
+      {isLoading && <UiSpinner />}
+      {isError && (
+        <div className="font-bold text-rose-500">
+          Ошибка при загрузке деталей патологии
+        </div>
+      )}
+      {tutorialDetails && (
+        <>
+          Тут будет видос
+          <UiTextArea className="mt-5">
+            {tutorialDetails.description}
+          </UiTextArea>
+          <UiLink href={ROUTES.ATLAS} className="mr-auto">
+            Назад
+          </UiLink>
+          <UiFooter activeStatus="atlas" />
+        </>
+      )}
+    </div>
+  );
+}
