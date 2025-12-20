52d5d5e84990fe618b54f309b14818e6ddc4ac5f
Denis <den.melnickov2001@yandex.ru>
2025-12-20 20:40:20 +0400

    feat: скелетон лоадер к кейсу

--- src/features/clinical-case/ui/case.tsx
+++ src/features/clinical-case/ui/case.tsx
@@ -8,7 +8,6 @@ import { UiFooter } from "@/shared/ui/ui-footer";
 import { ROUTES } from "@/shared/constants/routes";
 import clsx from "clsx";
 import { useCase } from "../model/use-case";
-import { UiSpinner } from "@/shared/ui/ui-spinner";
 import { useModal } from "@/shared/lib/use-modal";
 import { UiImageModal } from "@/shared/ui/ui-image-modal";
 
@@ -31,41 +30,52 @@ export function Case({ className }: { className?: string }) {
           "flex flex-col w-full gap-3 flex-1 mb-4 px-5 mt-5"
         )}
       >
-        {isLoading && <UiSpinner />}
         {isError && (
           <div className="font-bold text-rose-500">
             Ошибка при загрузке деталей патологии
           </div>
         )}
-        {caseDetails && (
-          <>
-            <UiScrollImg
-              img={caseDetails.imgContainer}
-              onIndexChangeAction={handleImageChange}
-            />
-            {currentImageIndex === caseDetails.imgContainer.length - 1 ? (
-              <img
-                src={caseDetails.imgSchema}
-                alt="Схематическое изображение"
-                loading="lazy"
-                fetchPriority="low"
-                decoding="async"
-                width={300}
-                height={285}
-                className="rounded-xl object-scale-down mt-3 w-full h-[40svh] cursor-zoom-in"
-                onClick={open}
-              />
-            ) : (
-              <UiTextArea className="mt-5" contentKey={currentImageIndex}>
-                {caseDetails.descriptionContainer[currentImageIndex]}
-              </UiTextArea>
-            )}
-            <UiLink href={ROUTES.CLINIC} className="mr-auto">
-              Назад
-            </UiLink>
-            <UiFooter activeStatus="clinic" />
-          </>
-        )}
+          {isLoading ? (
+              <>
+                  <UiScrollImg
+                      img={[]}
+                      onIndexChangeAction={handleImageChange}
+                      isLoading={true}
+                  />
+                  <UiTextArea className="mt-5" isLoading={true} />
+                  <UiFooter activeStatus="clinic" />
+              </>
+          ) : (
+              caseDetails && (
+                  <>
+                      <UiScrollImg
+                          img={caseDetails.imgContainer}
+                          onIndexChangeAction={handleImageChange}
+                      />
+                      {currentImageIndex === caseDetails.imgContainer.length - 1 ? (
+                          <img
+                              src={caseDetails.imgSchema}
+                              alt="Схематическое изображение"
+                              loading="lazy"
+                              fetchPriority="low"
+                              decoding="async"
+                              width={300}
+                              height={285}
+                              className="rounded-xl object-scale-down mt-3 w-full h-[40svh] cursor-zoom-in"
+                              onClick={open}
+                          />
+                      ) : (
+                          <UiTextArea className="mt-5" contentKey={currentImageIndex}>
+                              {caseDetails.descriptionContainer[currentImageIndex]}
+                          </UiTextArea>
+                      )}
+                      <UiLink href={ROUTES.CLINIC} className="mr-auto">
+                          Назад
+                      </UiLink>
+                      <UiFooter activeStatus="clinic" />
+                  </>
+              )
+          )}
       </div>
       <UiImageModal isOpen={isOpen} onClose={close}>
         {caseDetails && (
