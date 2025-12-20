d7f17ef70d21204b4a210586290c92369c77e147
Denis <den.melnickov2001@yandex.ru>
2025-12-20 20:45:41 +0400

    feat: скелетон лоадер к прохождению теста

--- src/app/passing-test/page.tsx
+++ src/app/passing-test/page.tsx
@@ -1,7 +1,5 @@
 import { TestTasks } from "@/features/test";
-import {Suspense} from "react";
 import { Metadata } from "next";
-import {UiSpinner} from "@/shared/ui/ui-spinner";
 
 export const metadata: Metadata = {
     title: "Тестирование",
@@ -11,9 +9,7 @@ export default function PassingTestPage() {
 
   return (
     <div className="flex flex-col w-full">
-        <Suspense fallback={<div className="w-full flex justify-center"><UiSpinner/></div>}>
             <TestTasks/>
-        </Suspense>
     </div>
   );
 }
