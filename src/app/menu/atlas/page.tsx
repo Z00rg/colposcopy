39122b9f0fe4628bf5cc3557c414b6ffc9f737ef
Z00rg <den.melnickov2001@yandex.ru>
2026-01-20 21:12:49 +0400

    feat: админ функции в основных разделах, определение роли по кукам

--- src/app/menu/atlas/page.tsx
+++ src/app/menu/atlas/page.tsx
@@ -1,12 +1,20 @@
 import {AtlasList} from "@/features/atlas";
 import {Metadata} from "next";
 import {UiFooter} from "@/shared/ui/ui-footer";
+import {cookies} from "next/dist/server/request/cookies";
 
 export const metadata: Metadata = {
     title: "Атлас",
 };
 
-export default function AtlasPage() {
+export default async function AtlasPage() {
+    // Куки до рендера на сервере
+    const cookieStore = await cookies();
+    const userRole = cookieStore.get("user_role")?.value;
+
+    // Проверка роли
+    const isAdmin = userRole === "admin";
+
     return (
         <>
             <div className="font-medium text-[20px] mt-7">
@@ -14,7 +22,7 @@ export default function AtlasPage() {
             </div>
 
             {/* Список патологий */}
-            <AtlasList/>
+            <AtlasList adminList={isAdmin}/>
 
             {/* Футер */}
             <UiFooter activeStatus="atlas"/>
--- src/app/menu/clinical-cases/page.tsx
+++ src/app/menu/clinical-cases/page.tsx
@@ -1,12 +1,20 @@
 import {ClinicalCasesList} from "@/features/clinical-case/ui/clinical-cases-list";
 import {Metadata} from "next";
 import {UiFooter} from "@/shared/ui/ui-footer";
+import {cookies} from "next/dist/server/request/cookies";
 
 export const metadata: Metadata = {
     title: "Клинические случаи",
 };
 
-export default function ClinicalCasesPage() {
+export default async function ClinicalCasesPage() {
+    // Куки до рендера на сервере
+    const cookieStore = await cookies();
+    const userRole = cookieStore.get("user_role")?.value;
+
+    // Проверка роли
+    const isAdmin = userRole === "admin";
+
     return (
         <>
             <div className="font-medium text-[20px] mt-7">
@@ -14,7 +22,7 @@ export default function ClinicalCasesPage() {
             </div>
 
             {/* Список патологий + клинических кейсов */}
-            <ClinicalCasesList/>
+            <ClinicalCasesList adminList={isAdmin}/>
 
             {/* Футер */}
             <UiFooter activeStatus="clinic"/>
