b8b231aebf27ebae6be1f074c5e9e79902411d8d
Z00rg <den.melnickov2001@yandex.ru>
2026-01-21 22:52:44 +0400

    feat: удален sitemap

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://atlascolposcopy.ru',
-    generateRobotsTxt: true,
-    changefreq: 'weekly',
-    priority: 0.7,
-};
